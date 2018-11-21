6048334fe18064b089f091806004f6e6326a51bf
ac <ac@domain.local>
2018-11-21 17:41:01 +0900

    add typescript

---
+++ src/pages/tsx-test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+export interface TestProps {}
+export default class Test extends React.Component<TestProps, any> {
+  public render() {
+    return <div>tsx test</div>
+  }
+}
